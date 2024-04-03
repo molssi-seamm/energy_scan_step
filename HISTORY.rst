4eb759caf585779a26ef2c5aa4bee2273c5a8fec
Paul Saxe <paul@thesaxes.net>
2024-04-03 07:07:24 -0400

    Initial commit after SEAMM plugin Cookiecutter creation, version '2024.4.3'

---
+++ HISTORY.rst
@@ -0,0 +1,8 @@
+=======
+History
+=======
+
+2024.4.3 (2024-04-03)
+---------------------
+
+* Plug-in created using the SEAMM plug-in cookiecutter.
---
+++ energy_scan_step/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+energy_scan_step
+A SEAMM plug-in for calculating energy profiles along coordinates
+"""
+
+# Bring up the classes so that they appear to be directly in
+# the energy_scan_step package.
+
+from .energy_scan import EnergyScan  # noqa: F401, E501
+from .energy_scan_step import EnergyScanStep  # noqa: F401, E501
+from .tk_energy_scan import TkEnergyScan  # noqa: F401, E501
+
+from .metadata import metadata  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+
+__author__ = "Paul Saxe"
+__email__ = "psaxe@molssi.org"
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
