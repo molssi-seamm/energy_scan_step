ebd5e45ce49d451cbaedeb992895ecca3e95d049
seamm <seamm@users.noreply.github.com>
2025-08-06 15:19:25 +0000

    Updating Docs for commit 3dd6640b57ad50e900b5f838a5dc8216e7965bdb made on 2025-08-06T15:16:58+00:00 from refs/tags/2025.8.6 by seamm

---
+++ dev/en/dev/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2025.8.5+1.g8f4a969',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
